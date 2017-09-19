405febf21b755fceb534536e9fc93a2045e727b4
madhuri09b <madhuribastavade@djalexander.in>
2017-09-19 16:54:05 +0530

    completed task 2 and task 3

--- src/phpreboot/tddworkshop1/Calculator.php
+++ src/phpreboot/tddworkshop1/Calculator.php
@@ -3,9 +3,38 @@ namespace Phpreboot\Tddworkshop1;
 
 class Calculator
 {
-	
-	public function add(){
+	//when no parameter
+	/* public function add(){
 		return 0;
-	}
+	} */ 
+	
+	//when single parameter
+	/* public function add($numbers = '')
+    {
+        if (empty($numbers)) {
+            return 0;
+        }
+
+        return intval($numbers);
+    } */
+	
+	//when multiple parameters
+	public function add($numbers = '')
+    {
+        if (empty($numbers)) {
+            return 0;
+        }
+		
+		 if (!is_string($numbers)) {
+            throw new \InvalidArgumentException('Parameters must be a string');
+        }
+		 $all_nums = explode(',', $numbers);
+		
+		if (array_filter($all_nums, 'is_numeric') !== $all_nums) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        }
+ 
+       return array_sum($all_nums); 
+    } 
 	
 }
--- tests/phpreboot/tddworkshop1/CalculatorTest.php
+++ tests/phpreboot/tddworkshop1/CalculatorTest.php
@@ -23,4 +23,38 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('integer', $result, 'Result of `add` is not an integer.');
     }
+	
+	public function testAddWithoutParameterReturnsZero()
+    {
+        $result = $this->calculator->add();
+        $this->assertSame(0, $result, 'Empty string on add do not return 0');
+    }
+	
+	 public function testAddWithSingleNumberReturnsSameNumber()
+    {
+        $result = $this->calculator->add('3');
+        $this->assertSame(3, $result, 'Add with single number do not returns same number');
+    }
+	
+	public function testAddWithTwoParametersReturnsTheirSum()
+    {
+        $result = $this->calculator->add('2,4');
+
+        $this->assertSame(6, $result, 'Add with two parameter do not returns correct sum');
+    }
+	
+	public function testAddWithThreeParametersReturnsTheirSum()
+    {
+        $result = $this->calculator->add('2,4,4');
+
+        $this->assertSame(10, $result, 'Add with three parameter do not returns correct sum');
+    } 
+	
+	/**
+     * @expectedException \InvalidArgumentException
+     */
+	public function testAddWithNonStringParameterThrowsException()
+    {
+        $this->calculator->add('5,a', 'Integer parameter do not throw error');
+    }
 }
